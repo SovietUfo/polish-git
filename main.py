b1b39f3bdf864b979dc3ebf97ce2f50a3ddb5273
SovietUfo <ommm@interia.pl>
2021-11-13 14:30:42 +0100

    updat

--- main.py
+++ main.py
@@ -18,7 +18,16 @@ translation = {
     "kłoda": "log",
     "zrestartuj": "reset",
     "chowaj": "stash",
-    "usuń": "rm"
+    "usuń": "rm",
+    "stan": "status",
+    "przynieś": "fetch",
+    "grep": "grep",
+    "przesuń": "mv",
+    "przywróć": "restore",
+    "przepołów": "bisect",
+    "pokaż": "show",
+    "przebazuj": "rebase",
+    "przełącznik": "switch"
 }
 
 args = sys.argv[1:]
@@ -29,4 +38,5 @@ first = args[0]
 if first in translation.keys():
     command = " ".join(str(x) for x in args).replace(first, translation[first])
     subprocess.run(["git", *command.split()])
-    
+else:
+    print("nie ma takiej komendy")    
